7bda79e4b38f3b36c6de432ca3b517bf99850413
Shaffer319 <michaelshaffer319@gmail.com>
2016-01-09 17:04:48 -0500

    Added an api file reader
    Added api file reader function and also added the API Key to the git
    ignore list

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
@@ -10,6 +11,7 @@ namespace Weather
 {
     class Program
     {
+        static String API_KEY = "";
         static void checkOpenWeatherAPI()
         {
             String query = "http://api.openweathermap.org/data/2.5/weather?q=London";
@@ -31,8 +33,37 @@ namespace Weather
 
         }
 
+        static bool b_running = true;
+
+        static void cancelHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            args.Cancel = false;
+            b_running = false;
+        }
+
+        static bool getAPIKey()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("API_KEY.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    API_KEY = sr.ReadToEnd();
+                   
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
+
         static void Main(string[] args)
         {
+            System.Console.CancelKeyPress += cancelHandler;
+
             checkOpenWeatherAPI();
         }
     }
